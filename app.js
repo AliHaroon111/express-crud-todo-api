2415fcba22f18be5cdb3f414f9e90613f2fa8acc
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-08-03 22:36:21 +0500

    Stage 0: hello server

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, server!");
+});
+
+export default app;
---
+++ server.js
@@ -0,0 +1,8 @@
+// server.js
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Task API listening on http://localhost:${PORT}`);
+});
